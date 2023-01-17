7ebd72440734219611e1570079ba8656bcf926fe
leekoby <abc2752@naver.com>
2023-01-17 20:23:27 +0900

    Update InputSample.js

--- vlpt/src/InputSample.js
+++ vlpt/src/InputSample.js
@@ -1,29 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 function InputSample() {
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: ''
+    });
+    const nameInput = useRef();
 
-    const [inputs, setInputs] = useState({ name: '', nickname: '' });
+    const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
 
-    const { name, nickname } = inputs;
-
-    const onChange = (event) => {
-        const { name, value } = event.target;
+    const onChange = e => {
+        const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
         setInputs({
-            ...inputs,
-            [name]: value,
+            ...inputs, // 기존의 input 객체를 복사한 뒤
+            [name]: value // name 키를 가진 값을 value 로 설정
         });
     };
+
     const onReset = () => {
         setInputs({
             name: '',
-            nickname: '',
-        })
+            nickname: ''
+        });
+        nameInput.current.focus();
     };
 
     return (
         <div>
-            <input name='name' placeholder="이름" onChange={onChange} value={name} />
-            <input name='nickname' placeholder="닉네임" onChange={onChange} value={nickname} />
+            <input
+                name="name"
+                placeholder="이름"
+                onChange={onChange}
+                value={name}
+                ref={nameInput}
+            />
+            <input
+                name="nickname"
+                placeholder="닉네임"
+                onChange={onChange}
+                value={nickname}
+            />
             <button onClick={onReset}>초기화</button>
             <div>
                 <b>값: </b>
