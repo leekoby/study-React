4ae0c92256f913c8651a1333f142e647ea1c9295
leekoby <abc2752@naver.com>
2023-01-24 21:17:59 +0900

    1

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
 function ExpenseItem() {
     return (
-        <div>
+        <div className="">
             <div>March 28th 2023</div>
             <div>
                 <h2>Car Insurance</h2>
                 <div>$299.99</div>
             </div>
         </div>
-    )
+    );
 }
 
 export default ExpenseItem;
