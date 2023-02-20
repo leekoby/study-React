7fb1c03fbaa48525151365265ebbd760fe4c5423
leekoby <abc2752@naver.com>
2023-02-20 15:59:19 +0900

    chapter 1

---
+++ doit_react_study/chapter1/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrParens: 'avoid',
+  semi: false,
+  printWidth: 90
+}
---
+++ doit_react_study/chapter1/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  console.log('App called.')
+  return <h1>Hello world!</h1>
+}
+
+export default App
