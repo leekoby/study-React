efead1ed60990b0c4e9f6958a822e90c97f540ca
leekoby <abc2752@naver.com>
2023-02-20 16:37:16 +0900

    sutdy chapter1

--- doit_react_study/chapter1/src/App.tsx
+++ doit_react_study/chapter1/src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import * as D from './data'
 
 function App() {
-  console.log('App called.')
-  return <h1>Hello world!</h1>
+  return (
+    <div>
+      <p>
+        {D.randomName()}, {D.randomJobTitle()}, {D.randomDayMonthYear()}
+      </p>
+      <img src={D.randomAvatar()} height="50" />
+      <img src={D.randomImage()} height="300" />
+    </div>
+  )
 }
 
 export default App
