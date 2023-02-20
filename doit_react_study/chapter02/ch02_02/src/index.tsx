4f6291c48c9f0ea85522c4e86f6e35f99553afba
leekoby <abc2752@naver.com>
2023-02-20 23:52:27 +0900

    study chapter02

--- doit_react_study/chapter02/ch02_02/src/index.tsx
+++ doit_react_study/chapter02/ch02_02/src/index.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootVirtualDOM = (
+  <ul>
+    <li>
+      <a href="http://www.google.com" target="_blank">
+        <p>go to google</p>
+      </a>
+    </li>
+  </ul>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(rootVirtualDOM)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
