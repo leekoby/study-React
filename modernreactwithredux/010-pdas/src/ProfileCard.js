5d0e1fa883c581538e08523da702b1409c488824
leekoby <abc2752@naver.com>
2023-02-24 01:41:41 +0900

    feat 010pdas

---
+++ modernreactwithredux/010-pdas/src/ProfileCard.js
@@ -0,0 +1,11 @@
+function ProfileCard({ title, handle }) {
+  // const { title, handle } = props;
+  return (
+    <div>
+      <div>Title is {title}</div>
+      <div>Handle is {handle}</div>
+    </div>
+  )
+}
+
+export default ProfileCard
