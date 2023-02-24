23316387befd90f78a82d06e7a7d968ab9f41c50
leekoby <abc2752@naver.com>
2023-02-24 21:14:59 +0900

    refact bulma css

--- modernreactwithredux/010-pdas/src/App.js
+++ modernreactwithredux/010-pdas/src/App.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css'
 import ProfileCard from "./ProfileCard";
 import AlexaImage from './images/alexa.png'
 import CortanaImage from './images/cortana.png'
@@ -6,12 +7,50 @@ import SiriImage from './images/siri.png'
 
 function App() {
   return (
-    <div>
-      <div>Personal Digital Assistants</div>
 
-      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
-      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
-      <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+    <div>
+
+      <section className='hero is-primary'>
+        <div className='hero-body'>
+          <p className='title'>
+            <div>Personal Digital Assistants</div>
+          </p>
+        </div>
+      </section>
+      <div className='container'>
+        <section className='section'>
+          <div className='columns'>
+            <div className='column is-3'>
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={AlexaImage}
+                description='Alexa was created by Amazon and helps you buy things'
+              />
+
+            </div>
+            <div className='column is-3'>
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                image={CortanaImage}
+                description='Cortana was made by Microsoft. Who knows what it does?'
+              />
+
+            </div>
+            <div className='column is-3'>
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                image={SiriImage}
+                description='Siri was made by Apple and is being phased out'
+              />
+
+            </div>
+          </div>
+        </section>
+      </div>
+
     </div>
   );
 
