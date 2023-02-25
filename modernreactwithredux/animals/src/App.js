2f2377f5d554978f6c36c5f92ba8bc8b771f0914
leekoby <abc2752@naver.com>
2023-02-25 17:04:17 +0900

    feat animal apllication

---
+++ modernreactwithredux/animals/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  const handleClick = () => {
+    console.log('Button was clicked!')
+  }
+  return (<div>
+    <button onClick={handleClick}>Add Animal</button>
+  </div>);
+}
+
+export default App;
